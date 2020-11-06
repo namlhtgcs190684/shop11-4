2b4ec81178bcab65be916895f41d8bdb01c4fde7
namlhtgcs190684 <71746846+namlhtgcs190684@users.noreply.github.com>
2020-11-06 23:35:24 +0700

    Update 31.01server.js

--- 31.01server.js
+++ 31.01server.js
@@ -2,30 +2,6 @@ http = require("http");
 
 const hostname = '0.0.0.0';
 const port =  process.env.PORT || 8080;
-
-/// ------------------ Khai bao LIB de su dung
-var express = require('express');
-var session = require('express-session');
-var app = express();
-
-/// ..................................................
-app.get('/', homePage);
-function homePage(req, res) {
-    res.send("HOME !");   
-    console.log("\n\t ... connect from ", req.connection.remoteAddress, req.headers.host);
-}
-
-/// ------------------ gọi SERVER thực thi
-
-
-var server = app.listen( port , function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("SERVER http://%s:%s", host, port)
-});
-);
-
 server.listen( port, hostname, 
     () => { console.log(' Server running: ',hostname, port ); }
 );
