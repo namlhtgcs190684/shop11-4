bb761d381b681dc595c88965427d427ed965f235
namlhtgcs190684 <71746846+namlhtgcs190684@users.noreply.github.com>
2020-11-06 23:37:45 +0700

    Update 31.01server.js

--- 31.01server.js
+++ 31.01server.js
@@ -2,6 +2,14 @@ http = require("http");
 
 const hostname = '0.0.0.0';
 const port =  process.env.PORT || 8080;
+server = http.createServer(
+    (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('chua co gi !');
+        res.end();
+    }
+);
 server.listen( port, hostname, 
     () => { console.log(' Server running: ',hostname, port ); }
 );
